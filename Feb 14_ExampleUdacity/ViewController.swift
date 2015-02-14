7757c4365f201cff866158ab78a88d9723b5f7bd
lilin3336298 <511403330@qq.com>
2015-02-14 20:39:41 +0900

    Initial Commit

---
+++ Feb 14_ExampleUdacity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Feb 14_ExampleUdacity
+//
+//  Created by Lin Li on 2/14/15.
+//  Copyright (c) 2015 OIST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
